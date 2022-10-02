b92500222a4d0955c5d053e715efbdfdbd279537
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2022-10-02 19:53:03 +0300

    feat(ui): add ordered list
    add three list items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ViewComponent } from './view/view.component';
 import { TodoViewComponent } from './todo-view/todo-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewComponent,
     TodoViewComponent
   ],
   imports: [
