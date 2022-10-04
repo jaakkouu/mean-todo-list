c305495420a694c1d33c5e4dcd89dcb7144e19f7
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2022-10-04 20:12:47 +0300

    chore(ui): relocate angular project

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'scope-enum': [2, 'always', ['ui', 'api', 'db', 'ci', 'docs', 'dev']]
-  }
+    "scope-enum": [2, "always", ["ui", "api", "db", "ci", "docs", "dev"]],
+  },
 };
