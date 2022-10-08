73485720b09939a46189fe0ca48258b3165e2821
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2022-10-08 17:37:27 +0300

    feat(api): add randomly generated mock data
    add cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+
+app.get("/todos", (req, res) => {
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+  const data = Array.from(Array(randomNumber), (_, index) => ({
+    name: `Todo ${index + 1}`,
+  }));
+  res.send(data);
 });
 
 app.listen(port, () => {
